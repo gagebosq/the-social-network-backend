b98641ceead6317c7e5d0fdf737e721dab6d9cb6
swagatron300 <gage.bosquez@gmail.com>
2025-01-21 18:01:25 -0600

    REST API view made

--- socialNetwork/tweets/views.py
+++ socialNetwork/tweets/views.py
@@ -1,13 +1,21 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
 from.models import Tweet
-# Create your views here.
+
+
 def home_view(request, *args, **kwargs):
     return HttpResponse("<h1>Hello world</h1>")
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    data = {
+    "id": tweet_id,
+    }
+    status = 200
     try:
         obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
     except:
-        raise Http404
-    return HttpResponse(f"<h1>Hello {tweet_id} - {obj.content} world</h1>")
+        data["message"] = "Not Found"
+        status = 404
+     
+    return JsonResponse(data, status = status)
